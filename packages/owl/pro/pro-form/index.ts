ab63bffe477388d63ecbe230d0f9209f0928ef20
zhangjl <330252125@qq.com>
2026-06-08 10:51:30 +0800

    feat: export pro-form composables and types, and upgrade antdv-next and icons dependencies

--- packages/owl/pro/pro-form/index.ts
+++ packages/owl/pro/pro-form/index.ts
@@ -1,1 +1,20 @@
 export { default as ProForm } from './pro-form.vue';
+export {
+  createProForm,
+  extendProForm,
+  proFormInternalKey,
+  provideProForm,
+  useInjectProForm
+} from './composables/create-pro-form';
+export { proFormConfigInjectionKey, type ProFormConfig } from './context';
+export { useProForm } from './use-pro-form';
+export type {
+  CreateProFormOptions,
+  CreateProFormReturn,
+  EventHookOn,
+  ExtendProForm,
+  InternalPath,
+  ProFormRegisteredInst
+} from './composables/create-pro-form';
+export type { ProFormExtendProps, ProFormProps, ValidateError, ValidationTrigger } from './props';
+export type { ProFormSlots } from './slots';
